aeb819ddcbfdd28fdaa80d59e4204984d4a35551
Ulises Teyechea <uteyechea@gmail.com>
2021-11-18 11:43:10 -0700

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tutorial used to train a new student working for his MS thesis. ',
+    author='Ulises Teyechea',
+    license='MIT',
+)
